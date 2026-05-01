dc497f1698fef063d39edcf39935d2a8d6809088
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-02 03:56:57 +0530

    type casting update

--- first.py
+++ first.py
@@ -1,8 +1,19 @@
-a=10
-b=20
-c=a+b
-d=c+54
-print(a)
-print(b)
-print(c)
-print(d)
+# a=10
+# b=20
+# c=a+b
+# d=c+54
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# type casting and type conversion
+
+# n=10.0
+# m=10.0
+# print(n+m)
+
+num="100"
+name="nikhil"
+print(num+" "+name)
+print(type(name))

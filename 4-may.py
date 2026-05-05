fbca929138c23c384049b08d2dcca2e7245ee96c
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-05 13:29:07 +0530

    4 may practice

--- 4-may.py
+++ 4-may.py
@@ -42,6 +42,13 @@ contact {student_number}"""
 # ==
 #!=
 
-str1="N"
-str2="s"
-print(str1>str2)
+# str1="N"
+# str2="s"
+# print(str1>str2)
+
+# # 4 .Logical opr (and,or,not)
+# n1=1
+# n2=3
+# # res=n1==1 and n2==3 and n1==2 and n2==3
+# res=not(n1==1 or n2==3 or n1==2 or n2==3)
+# print(res)

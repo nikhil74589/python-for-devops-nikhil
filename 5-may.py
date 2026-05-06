91fb9eb6872ef1456921ec6c9d38bf180a2afe10
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-06 08:58:19 +0530

    5 may practice

---
+++ 5-may.py
@@ -0,0 +1,38 @@
+# 5 Membership opr (in , not in)
+
+# str1 ="this is python for devops"
+# find="python"
+# print(find in str1)
+
+# num1="4567"
+# print("5"in num1)
+
+# email="iqindia123@gmail.com"
+# find="@gmail.com"
+# print(find in email)
+
+# email="iqindia123@gmail.com"
+# find="@gmail.com"
+# if find in email:
+#     print("This is valid google mail id")
+# else:
+#     print("Invalid email") 
+
+
+# Aman_age=20
+# Abhishek_age=30
+# if Aman_age > Abhishek_age:
+#     print("Yes Aman's age is more than Abhishek")
+# else:
+#     print(" No Aman's age is not more than Abhishek")  
+
+#  User is eligible for voting.
+# min_age=18
+# nationality="IN"
+# user_age=int(input("Enter your age :"))
+# user_id=input("Enter your identity (ex.IN)")
+# if min_age <= user_age: and user_id==nationality
+#     print("User is not eligible for voting")
+# else:
+#     print("User is not eligible for votuing")
+

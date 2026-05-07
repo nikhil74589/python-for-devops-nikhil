08776809184c367c97af032650a14a7205b98307
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-07 08:46:43 +0530

    practice class 6 may

---
+++ 6-may.py
@@ -0,0 +1,96 @@
+# 6.Identity opr (is , is not)
+# a=100
+# b=100
+# print(id(a))
+# print(id(b))
+# print (a is  not b)
+
+
+# Conditional statement
+
+# a=0
+# if a :
+#     print("Yes")
+# else :
+#     print ("No")
+
+# a=""
+# if a :
+#     print("Yes")
+# else :
+#     print ("No")
+
+# a=True
+# b=True
+# print(a+b)
+
+# a=True
+# b=False
+# print(a+b)
+
+# a=True
+# b=True+True
+# print(a*b)
+
+# a=True
+# b=True
+# print(type(a))
+# print(a*b)
+
+# if 1*1*0 :
+#     print("Yes")
+# else :
+#     print("No")    
+
+# if True*0 :
+#     print("Yes")
+# else :
+#     print("No") 
+
+# if True*True :
+#     print("Yes")
+# else :
+#     print("No")   
+
+# wap to check number given by user is odd or even.
+# num=23
+# if num % 2 == 0 :
+#     print("Even")
+# else :
+#     print ("Odd")
+
+# num=22
+# if num % 2 == 0 :
+#     print("Even")
+# else :
+#     print ("Odd")
+
+# num=int(input("Enter Your number :"))
+# if num % 2 == 0 :
+#     print("Even")
+# else :
+#     print ("Odd")
+
+# wap to print the last digit of and number is 456735
+# print(456735%10)
+# print(4567355%1000)
+# print(256005%1000)
+
+
+# wap to find largest
+# wap to find smallest
+# num1=3
+# num2=9
+# num3=12
+
+# if  num1>=num2 and num1>=num3 :
+#     print(f"{num1} is largest")
+
+# if  num2>=num1 and num2>=num3 :
+#     print(f"{num2} is largest")
+
+# if  num3>=num1 and num3>=num2 :
+#     print(f"{num3} is largest")
+
+
+    

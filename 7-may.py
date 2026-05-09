fb54970fbb617ade2526238856fbe556f49dc3b8
nikhil74589 <nikhilranjan2501@gmail.com>
2026-05-09 11:10:50 +0530

    New file inserted

---
+++ 7-may.py
@@ -0,0 +1,71 @@
+# name="Dev"
+# if name =="Dev" :
+#     print("Yes name is provided")
+# else :
+#     Print("Name is not provided")    
+
+# name=""
+# if name =="Dev" :
+#     print("Yes name is provided")
+# else :
+#     Print("Name is not provided") 
+
+# name="Aman"
+# if name :
+#     print("Yes name is provided")
+# else :
+#     Print("Name is not provided") 
+
+
+# name=""
+# if name :
+#     print("Yes name is provided")
+# else :
+#     Print("Name is not provided")    
+
+# name ="Nikhil"
+# if name :
+#      print(f"Yes name {name} is provided")
+#      address = "Noida"
+#      if address:
+#           print(f"Yes address is provided: {address}")
+
+#      else : 
+#         print(f"Address is not provided") 
+        
+# else :
+#     Print("Name is not provided")
+
+# num1 = 20
+# if num1%2 == 0:
+#     print ("EVEN")
+#     mob = input("Enter your number :")
+#     if len (mob) == 10 :
+#         print("Valid number")
+#     else :
+#         print("Invalid number")
+# else :
+#     print("ODD")  
+
+# student_name = input("Enter your name :")
+# pre =int(input("Enter your pre marks :"))
+# if pre >= 400 :
+#     print ("You are pass in pre and eligible for Mains")
+
+#     mains = int(input("Enter your mains marks :"))  
+#     if mains >= 600 :
+
+#         print("You are pass in mains and eligible for Interview ")
+
+#         inter =int(input("Enter your interview marks :"))
+
+#         if inter >= 700 :
+#             print(f"You are selected as IAS Mr. {student_name}")
+
+#         else :
+#             print(f"You are fail in interview Mr.{student_name}")   
+#     else :
+#         print("Fail in mains")    
+
+# else :
+#     print("Better luck next time , pre failed ❌") 
---
+++ 8-may.py
@@ -0,0 +1,107 @@
+# 1.for loop : ranged based
+# 2.while loop : condition based
+
+# 1.for loop 
+# range always works with integer number.
+# range (start:0 ,stop-1 ,step:1)
+
+# for i in range (10):
+#      print("Nikhil")
+
+# for i in range (0,5,1):
+#     print(i)
+
+# for i in range (0,5,2):
+#     print(i)
+
+# for i in range (0,7,2):
+#     print(i)
+
+# for i in range (1,16,1):
+#     print(i)
+
+# for i in range (1,16,2):
+#     print(i,end = " ")
+
+# for i in range (1,20):
+#     if i == 10 :
+#         break
+#     print (i)
+
+# for i in range (1,20):
+#     if i == 10 :
+#         continue
+#     print (i)
+
+# for i in range (1,20):
+#     if i%2 == 0 :
+#         print (i,end = " ")
+
+# for i in range (1,21):
+#     if i%2 == 0 :
+#        print (i,end = " ")
+
+# for i in range (1,21):
+#     if i%2 != 0 :
+#        print (i,end = " ")
+
+# for i in range (1,21):
+#     if i%2 != 0 :
+#        print (f"Odd : {i}")
+#     else :
+#         print(f"Even : {i}")
+
+# for sum
+# s=0  
+# for i in range (1,5):
+#     s = s + i
+#     print(s)
+
+# s=1  
+# for i in range (1,5):
+#     s = s * i
+#     print(s)
+
+# s=1  
+# for i in range (1,5):
+#     s = s * i
+# print(s)
+
+# for i in range (10,1,-1):
+#     print(i)
+
+# for i in range (10,0,-1):
+#     print(i)
+
+# for r in range (10,-3,-1):
+#     print(r)
+
+
+
+
+
+# wap to takes start_point and end_point from user input and print all number divisible by 2 and 3.
+
+# start = int(input("Enter the start point:"))
+# end = int(input("Enter the end point:"))
+# print(f"Numbers between {start} and {end} divisible by both 2 and 3:" )
+
+# for num in range (start, end + 1):
+#     if num % 2 == 0 and num % 3== 0:
+#         print (num)
+
+
+    
+# wap to take a number from user_input and print formated table.
+# format :
+
+# num = int(input("Enter the number :"))
+# for i in range (1,11) :
+#    print (f"{num}*{i} = {num * i}")
+
+
+# wap to take a number from user_input and print reversed formated table.
+
+# num = int(input("Enter the number :"))
+# for i in range (10,0,-1) :
+#    print (f"{num}*{i} = {num * i}")
